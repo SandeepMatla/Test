e4d7a7eb8fe2641f23c4d65d1f4d2ef91234a332
Sandeep Matla <122857074+SandeepMatla@users.noreply.github.com>
2023-02-23 22:58:37 +0530

    love

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello , Fuck you!");
+        System.out.println("Hello , love you!");
     }
 }
